eb7681cf99849fc3c0212045c9421e39fa6fc294
Monitha721 <monithadhanabalan476@gmail.com>
2026-03-24 11:13:20 +0530

    UC3

--- App/src/BookMyStayApp.java
+++ App/src/BookMyStayApp.java
@@ -1,6 +1,20 @@
-public class BookMyStayApp {
-    public static void main(String[] args){
-        System.out.println("Welcome to the Hotel Booking Management System");
-        System.out.println("System initialized successfully.");
+public class UseCase4RoomSearch {
+
+    public static void main(String[] args) {
+
+        Room singleRoom = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suiteRoom = new SuiteRoom();
+
+        RoomInventory inventory = new RoomInventory();
+
+        RoomSearchService searchService = new RoomSearchService();
+
+        searchService.searchAvailableRooms(
+                inventory,
+                singleRoom,
+                doubleRoom,
+                suiteRoom
+        );
     }
 }
